e38ea71a785937673f29fffd1f376ea99f5042fe
Siwady <edward@acklenavenue.com>
2017-12-07 11:46:50 -0600

    adding ngrx to the app.module.ts and one example of actions  and reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule, routerReducer } from '@ngrx/router-store';
 
 import { AppComponent } from './app.component';
+import { appReducer } from './app.reducer';
 
 
 @NgModule({
@@ -10,7 +14,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      // routes
+    ]),
+    StoreModule.forRoot({ app: appReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 50
+    }),
+    StoreRouterConnectingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
