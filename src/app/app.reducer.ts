6056963573016d9ebf506d6ca0e0076f3b068c14
Siwady <edward@acklenavenue.com>
2017-12-07 14:33:34 -0600

    added prettier

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,30 +1,31 @@
-import { Action } from '@ngrx/store';
-import * as app from './app.action';
-import { InitialState } from '@ngrx/store/src/models';
+import { Action } from "@ngrx/store";
+import * as app from "./app.action";
+import { InitialState } from "@ngrx/store/src/models";
 
 export interface State {
-    votes: number;
+  votes: number;
 }
 
 export const initialState: State = {
-    votes: 0
+  votes: 0
 };
 
 export function appReducer(state = initialState, action: app.Actions) {
-    let value = 0;
-    switch (action.type) {
-        case app.INCREMENT:
-            value = action.payload;
-            return {...state, votes: state.votes + value};
+  let value = 0;
+  switch (action.type) {
+    case app.INCREMENT:
+      value = action.payload;
 
-        case app.DECREMENT:
-            value = action.payload;
-            return {...state, votes: state.votes - value};
+      return { ...state, votes: state.votes + value };
 
-        case app.RESET:
-            return {...state, votes: 0};
+    case app.DECREMENT:
+      value = action.payload;
+      return { ...state, votes: state.votes - value };
 
-        default:
-            return state;
-    }
+    case app.RESET:
+      return { ...state, votes: 0 };
+
+    default:
+      return state;
+  }
 }
